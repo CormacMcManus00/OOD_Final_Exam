79b3896c9af097e0e1ca60311f97cd6edfeadb2e
Cormac McManus <60261833+CormacMcManus00@users.noreply.github.com>
2021-05-11 12:45:05 +0100

    Add files via upload

---
+++ NUnitTest/DecreasePriceTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using CormacMcManus_S00197425;
+
+namespace NUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
